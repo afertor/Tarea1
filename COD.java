c71019a37686b51f630020b2bf4b5e682ba9bc5e
Adrian <afernandeztorres@danielcastelao.org>
2022-11-15 19:24:15 +0100

    Primer commit

---
+++ COD.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cod;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Clase2 {
+     public void metodo2(){
+    System.out.println("adios");
+}
+}
